8fb4bf04398c96566e6de267cec6c139df9397f7
dell <dell@DESKTOP-3N0O4DU>
2024-01-06 16:50:00 +0530

    A

---
+++ MavenA/src/test/java/com/actitime/testscrpit/CreateModule.java
@@ -0,0 +1,12 @@
+package com.actitime.testscrpit;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateModule {
+	@Test
+public void CreateCustomer()
+{
+	Reporter.log("CreateCustomer",true);
+}
+}
